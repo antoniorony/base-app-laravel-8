9739156e6675e24c7587c0527d10224ce021f67d
antoniorony.dsn.pack <antoniorony.dsn.pack@alterdata.com.br>
2022-05-17 17:04:54 -0300

    Adição de ajustes visuais

--- resources/views/inicial.blade.php
+++ resources/views/inicial.blade.php
@@ -37,30 +37,25 @@
               </div>
             </div>
           </nav>
-        <section>
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="http://apycom.com/bootstrap-components/data/upload/2017/03/1slides-only.jpg" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="http://apycom.com/bootstrap-components/data/upload/2017/03/1slides-only.jpg" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="http://apycom.com/bootstrap-components/data/upload/2017/03/1slides-only.jpg" class="d-block w-100" alt="...">
-                  </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
+        <section class="container">
+          <div class="row">
+            <div class="col-6">
+              <div class="slide-centrall">
+                @include('componentes.carousel.carousel')
               </div>
+            </div>
+            <div class="col-4">
+              <div class="slide-centrall">
+                @include('componentes.carousel.carousel')
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="row">
+          @include('componentes.card.card')
+          @include('componentes.card.card')
+          @include('componentes.card.card')
         </section>
     </div>
-    @endsection
 @extends('footer.footer')
 
